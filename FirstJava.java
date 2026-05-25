6d9119d1267515d1f499477bcfcb350fe111e815
shweta <shwetavishwkarma04@gmail.com>
2026-05-25 13:28:03 +0530

    evenodd

--- FirstJava.java
+++ FirstJava.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 //product name,quantity,price
 class FirstJava{
-    public static int numberSquare(int a){
-        int square = a * a;
-        return square;
+    public static int evenOdd(int a){
+        if (a%2 == 0){
+            System.out.println("the number is even");
+        }else{
+            System.out.println("the number is odd");
+        }
+
+        return a;
     }
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
           
-        int sum =  numberSquare(a);
-        System.out.println("square of number" +sum);
+        int sum =  evenOdd(a);
+        
 
 } }
