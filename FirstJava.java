87b07287e920d7b3539c75630e24920087ff975a
shweta <shwetavishwkarma04@gmail.com>
2026-05-25 13:18:56 +0530

    2qution done

--- FirstJava.java
+++ FirstJava.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 //product name,quantity,price
 class FirstJava{
-    public static void greet(){
-        System.out.println("Welcome to Java");
+    public static int numberSums(int a, int b){
+        int sum = a + b;
+        return sum;
     }
     public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt(); 
+          
+        int sum =  numberSums(a , b);
+        System.out.println("sum of number" +sum);
 
-  greet();
-    
 } }
