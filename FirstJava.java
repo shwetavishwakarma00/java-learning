08065d86465b33ffafe8b2a610e129ff6f94d347
shweta <shwetavishwkarma04@gmail.com>
2026-05-25 13:50:28 +0530

    largest number

--- FirstJava.java
+++ FirstJava.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 //product name,quantity,price
 class FirstJava{
-    public static int evenOdd(int a){
-        if (a%2 == 0){
-            System.out.println("the number is even");
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st number:");
+        int a = sc.nextInt();
+        System.out.println("enter 2nd number:");
+        int b = sc.nextInt();
+        System.out.println("enter 3rd number:");
+        int c = sc.nextInt();
+
+        largeNumber(a, b, c);
+
+} 
+    public static int largeNumber(int a, int b, int c){
+        if(a>b && a>c){
+            System.out.println(a+ "is gratest number");
+        } else if(b>a && b>c){
+            System.out.println(b+ "is gratest number");
+        }else if(c>a && c>b){
+            System.out.println(c+ "is gratest number");
+        }else if(a==b && b==c && a==c){
+            System.out.println("all numbers are equal");
         }else{
-            System.out.println("the number is odd");
+            System.out.println("invalid number");
         }
 
         return a;
     }
-    public static void main (String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-          
-        int sum =  evenOdd(a);
-        
 
-} }
+}
