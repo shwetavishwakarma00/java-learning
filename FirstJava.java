46513536a22f95fbcfc123e0624e8c2295ff9049
shweta <shwetavishwkarma04@gmail.com>
2026-05-25 13:03:18 +0530

    functionsPractice1

--- FirstJava.java
+++ FirstJava.java
@@ -1,26 +1,11 @@
 import java.util.Scanner;
 //product name,quantity,price
 class FirstJava{
+    public static void greet(){
+        System.out.println("Welcome to Java");
+    }
     public static void main (String args[]){
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter username:");
-    String username = sc.nextLine();
-
-    
-
-   if(username.equals("admin")){
-    System.out.println("Enter OTP:");
-    int otp = sc.nextInt();
-    if(otp == 9878){
-        System.out.println("Login successful");
-    }
-    else{
-        System.out.println("Invalid OTP");
-    }
-   }else{
-    System.out.println("Invalid username");
-   }
-
+  greet();
     
 } }
