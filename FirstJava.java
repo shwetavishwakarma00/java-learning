a4fca6055c6bb8584b6ee731df5c09f5a0471c15
shweta <shwetavishwkarma04@gmail.com>
2026-05-25 13:22:05 +0530

    3rd is done

--- FirstJava.java
+++ FirstJava.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 //product name,quantity,price
 class FirstJava{
-    public static int numberSums(int a, int b){
-        int sum = a + b;
-        return sum;
+    public static int numberSquare(int a){
+        int square = a * a;
+        return square;
     }
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt(); 
           
-        int sum =  numberSums(a , b);
-        System.out.println("sum of number" +sum);
+        int sum =  numberSquare(a);
+        System.out.println("square of number" +sum);
 
 } }
