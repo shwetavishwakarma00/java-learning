059f20fbfa5f86f64521cf8a62988799dd210ee6
shweta <shwetavishwkarma04@gmail.com>
2026-05-26 12:05:27 +0530

    login is done

--- MiniAtm.java
+++ MiniAtm.java
@@ -4,25 +4,47 @@ public class MiniAtm {
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter number: ");
-        int a = sc.nextInt();
-        primeChecker(a);
+        System.out.println("Enter your user name: ");
+        String userName = sc.nextLine();
+        System.out.println("Enter your password");
+        String pass = sc.nextLine();
+
+        login(userName, pass);
 
     }
- public static void primeChecker(int a){
-    boolean isPrime = true;
+ public static boolean checkUsername(String userName){
+  
+   if (userName.equals("ShwetaV")){
+    System.out.println("thank u now enter password");
+    return true;
+   }else{
+    System.out.println("userid is wrong, try again");
+    return false;
+   }
+   
+ }
 
-    for(int i = 2; i <= a/2; i++){
-        if(a % i == 0 ){
-            isPrime = false;
-            break;
-        }
-    }
-    if(isPrime){
-        System.out.println("prime number");
-    }else {
-        System.out.println("not Prime number");
-    }
+ public static boolean checkPassword(String pass){
+ 
+ if(pass.equals("Shweta@123")){
+ System.out.println("loading");
+ return true;
+ }else{
+  System.out.println("wrong password");
+  return false;
+ }
+ 
+ }
+
+ public static void login( String userName, String pass){
+ boolean userOk = checkUsername(userName);
+ boolean userPass = checkPassword(pass);
+
+  if ( userOk &&  userPass){
+    System.out.println("login successful");
+  }else{
+    System.out.println("not allowed");
+  }
  }
 }
 
