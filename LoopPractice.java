e5e0d48e95405201c58c3ed25aefb292f6c69ee8
shweta <shwetavishwkarma04@gmail.com>
2026-05-28 16:38:08 +0530

    factorial

--- LoopPractice.java
+++ LoopPractice.java
@@ -5,10 +5,11 @@ public class LoopPractice {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = sc.nextInt();
-        int mul = 1;
-        for(int i= 1; i<=10; i++){
-            mul = num * i ;
-            System.out.println(mul);
+        int factorial = 1;
+        for(int i= 1; i<=num; i++){
+            factorial = factorial * i ;
         }
+        System.out.println(factorial);
+
     }
 }
