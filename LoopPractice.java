c5e91d783a5e1e43522b86353bff46f0441d4734
shweta <shwetavishwkarma04@gmail.com>
2026-05-28 16:32:03 +0530

    numbers table

--- LoopPractice.java
+++ LoopPractice.java
@@ -5,13 +5,10 @@ public class LoopPractice {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = sc.nextInt();
-        int sum = 0;
-        for(int i=0; i <= num; i++ ){
-            
-            sum  = sum  + i;
+        int mul = 1;
+        for(int i= 1; i<=10; i++){
+            mul = num * i ;
+            System.out.println(mul);
         }
-         System.out.println(sum);
-
-        
     }
 }
