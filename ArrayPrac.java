9235006c3d1dc510f2e5a683263b027223e498b4
shweta <shwetavishwkarma04@gmail.com>
2026-06-01 16:29:25 +0530

    sum of array

--- ArrayPrac.java
+++ ArrayPrac.java
@@ -2,18 +2,14 @@ import java.util.Scanner;
 
 public class ArrayPrac {
     public static void main(String args[]){
-        int[] values = new int[5];
+        int[] arr = {1,2,9,12};
+        int sum =0;
 
-        Scanner sc = new Scanner(System.in);
+        for(int i=0; i < arr.length; i++){
+             sum += arr[i];
+            
+        }
+        System.out.println(sum);
         
-
-        for(int i = 0; i< values.length; i++){
-        System.out.println("Enter a number: ");
-        values[i] =  sc.nextInt();       
-     }
-     System.out.println("numbers are: ");
-     for(int i = 0; i<values.length; i++){
-        System.out.println(values[i]);
-     }
     }
 }
