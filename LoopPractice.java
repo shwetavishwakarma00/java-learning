7bb5712c9a6d60643bef476b94dd07def57e9b5f
shweta <shwetavishwkarma04@gmail.com>
2026-06-01 11:35:31 +0530

    fibbonesis

--- LoopPractice.java
+++ LoopPractice.java
@@ -2,21 +2,17 @@ import java.util.Scanner;
 
 public class LoopPractice {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int num = sc.nextInt();
-        boolean isPrime = true;
-
-        for( int i = 2; i <= num/2; i++){
-            if( num % i == 0){
-               isPrime = false;
-               System.out.println("not prime");
-               break; 
-            }else{
-                isPrime = true;
-                System.out.println("number is prime");
-            }
+        int first = 0;
+        int second = 1;
+        
+        for( int i = 0; i <= 50; i++){
+           int next = first + second;
+           System.out.println(next);
+           first = second;
+           second = next;
         }
+            
+
         
     }
 }
