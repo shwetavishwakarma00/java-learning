bf56aa1eaa02dbcff318fe337729cf22da5bbb2f
shweta <shwetavishwkarma04@gmail.com>
2026-06-10 12:35:48 +0530

    Strings1

---
+++ Strings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String word = sc.nextLine();
+        System.out.println(word.length());
+    }
+}
