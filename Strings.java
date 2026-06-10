8bb24d3c14592df0786cc861f9608a054c2ba48a
shweta <shwetavishwkarma04@gmail.com>
2026-06-10 13:05:45 +0530

    strings4

--- Strings.java
+++ Strings.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 public class Strings {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the word: ");
+        System.out.println("Enter the sentance: ");
         String word = sc.nextLine();
-        System.out.println(word.toUpperCase());
+        
+        if(word.contains("Java")){
+            System.out.println("Java found");
+        }else{
+            System.out.println("Not found");
+        }
 
     }
 }
