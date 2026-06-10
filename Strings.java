fe83633d5481e014488f595ed3385bbd55614e17
shweta <shwetavishwkarma04@gmail.com>
2026-06-10 15:23:45 +0530

    string 10

--- Strings.java
+++ Strings.java
@@ -3,18 +3,10 @@ import java.util.Scanner;
 public class Strings {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the word: ");
-        String word = sc.nextLine();
-        System.out.println("Enter a character: ");
-        char chr = sc.next().charAt(0);
-        int count = 0;
+        System.out.println("Enter the sentance: ");
+        String sentance = sc.nextLine();
         
-        for(int i=0; i<word.length(); i++){
-            char ch = word.charAt(i);
-            if(ch == chr){
-                count++;
-            }
-        }
-        System.out.println(count);
+        String[] word = sentance.trim().split("\\s+");
+        System.out.println(word.length);
     }
 }
