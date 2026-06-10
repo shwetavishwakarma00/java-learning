efddf5c6b6ee4dcb62efe0c6450f412a0b5b3e9d
shweta <shwetavishwkarma04@gmail.com>
2026-06-10 12:52:57 +0530

    strings2

--- Strings.java
+++ Strings.java
@@ -5,6 +5,9 @@ public class Strings {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the word: ");
         String word = sc.nextLine();
-        System.out.println(word.length());
+        System.out.println(word.charAt(0));
+        int strInd = word.length()-1;  // beacuse it gives chater count not index count
+        System.out.println(word.charAt(strInd));
+
     }
 }
