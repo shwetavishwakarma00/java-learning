90dff40ecb78ffac00786aba11454554b4c7bda3
shweta <shwetavishwkarma04@gmail.com>
2026-06-10 14:39:06 +0530

    sojjlk

--- Strings.java
+++ Strings.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.Scanner;
-
 public class Strings {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -8,10 +7,8 @@ public class Strings {
         String word = sc.nextLine();
         int upperCase = 0;
         int lowerCase = 0;
-
         for(int i=0; i<(word.length()); i++){
             char ch = word.charAt(i);
-        
             if(Character.isUpperCase(ch)){
                 upperCase++;
             }else{
@@ -20,6 +17,5 @@ public class Strings {
         }
         System.out.println("UpperCase: "+upperCase);
         System.out.println("LowerCase: "+lowerCase);
-
     }
 }
