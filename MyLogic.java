48481e7695ea607d93458d3a44f09e0061cf5d78
shweta <shwetavishwkarma04@gmail.com>
2026-06-11 14:12:59 +0530

    .

--- MyLogic.java
+++ MyLogic.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class MyLogic {
     public static void main(String args[]){
-        //User se number lo aur check karo even hai ya odd.
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number: ");
         int num = sc.nextInt();
