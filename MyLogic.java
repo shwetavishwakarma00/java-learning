ee4c940d78fdedde4cead72a54053711dc61af4a
shweta <shwetavishwkarma04@gmail.com>
2026-06-11 13:30:36 +0530

    Q1

--- MyLogic.java
+++ MyLogic.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class MyLogic {
-    
+    public static void main(String args[]){
+        //User se number lo aur check karo even hai ya odd.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num%2 == 0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
 }
