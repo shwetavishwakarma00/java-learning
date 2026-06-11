0d9821b917937db963f79b6c5d04f927b2b90966
shweta <shwetavishwkarma04@gmail.com>
2026-06-11 14:12:38 +0530

    Q3

--- MyLogic.java
+++ MyLogic.java
@@ -4,13 +4,11 @@ public class MyLogic {
     public static void main(String args[]){
         //User se number lo aur check karo even hai ya odd.
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter age: ");
-        int age = sc.nextInt();
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
 
-        if(age >= 18  ){
-            System.out.println("eligeblwe for vote");
-        }else{
-            System.out.println("not eligible");
+        for(int i = 1; i <= num; i++){
+            System.out.println(i);
         }
     }
 }
