681d0bd5f37eb73758f99cb781c7938f990cae4f
shweta <shwetavishwkarma04@gmail.com>
2026-06-11 14:34:52 +0530

    Q6

--- MyLogic.java
+++ MyLogic.java
@@ -3,14 +3,25 @@ import java.util.Scanner;
 public class MyLogic {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        int num = sc.nextInt();
-        int mul = 0;
+        System.out.println("Enter 1st number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter 2nd number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter 3rd number: ");
+        int num3 = sc.nextInt();
 
-        for(int i = 1; i <= 10; i++){
-            mul = num * i;
-            System.out.println(mul);
+        if(num1 > num2 && num1 > num3){
+            System.out.println(num1+ "i a gratest number");
+        }else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2+ "i a gratest number");
+        }else if (num3 > num1 && num3 > num2) {
+            System.out.println(num3+ "i a gratest number");  
+        }else if (num1 == num2 && num2 == num3) {
+            System.out.println("all are equals");
+        }else{
+            System.out.println("invalid number");
         }
+       
         
     }
 }
