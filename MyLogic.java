a87703b276f420e75c51a44853dbf2ac825b308b
shweta <shwetavishwkarma04@gmail.com>
2026-06-11 14:57:22 +0530

    Q9

--- MyLogic.java
+++ MyLogic.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class MyLogic {
     public static void main(String args[]){
       int[] arr = {1,2,3,4,5,6,7,8};
+      int sum = 0;
 
-      System.out.println(Arrays.toString(arr));
+      for(int num : arr){      //Array ke har element ko ek-ek karke num me store karo
+        sum += num;
+      }
+      System.out.println(sum);
 }
 }
