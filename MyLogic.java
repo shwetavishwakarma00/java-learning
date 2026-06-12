427fceeda6356d8c9b30ab2bd12e1e02d3f805c6
shweta <shwetavishwkarma04@gmail.com>
2026-06-12 12:47:44 +0530

    Q13

--- MyLogic.java
+++ MyLogic.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class MyLogic {
     public static void main(String args[]){
      int[] arr = {1,2,3,4,5,69};
-     int gratest = arr[0];
+     int smallest = arr[0];
 
      for(int num : arr){
-        if(num > gratest){
-           gratest = num; 
+        if(num < gratest){
+           smallest = num; 
         }
      }
-     System.out.println("graest element: " +gratest);
+     System.out.println("graest element: " +smallest);
 
 }
 }
