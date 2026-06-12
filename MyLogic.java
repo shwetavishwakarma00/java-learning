953e436f11196309bef4df70cddf16d5fee95449
shweta <shwetavishwkarma04@gmail.com>
2026-06-12 16:20:20 +0530

    Q17

--- MyLogic.java
+++ MyLogic.java
@@ -8,12 +8,21 @@ public class MyLogic {
      System.out.println("Enter the word: ");
      String word = sc.nextLine();
      char ch = ' ';
+     int upperCase = 0;
+     int lowerCase = 0;
+
      for(int i = 0; i < word.length(); i++){
-         ch = word.charAt(i);
-         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-            System.out.println(ch+ " vowels");
-         }
+        ch = word.charAt(i);
+        if(Character.isUpperCase(ch)){
+            upperCase++;
+        }else if(Character.isLowerCase(ch)){
+            lowerCase++;
+        }
      }
+     System.out.println("UpperCase: " +upperCase);
+     System.out.println("LowerCase: " +lowerCase);
+
+
      
 
 }
